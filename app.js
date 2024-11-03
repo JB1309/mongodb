fb685eab79cddd1dffc395fc05d18d4488eec9cf
John Basipaka <johnbasipaka2003@gmail.com>
2024-11-04 00:03:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const empRouter =require('./controller/emp_data')
+
+//url
+
+const url = 'mmongodb://localhost:27020,localhost:27021,localhost:27022/cbit2?replicaSet=m101';
+const app = express()
+mongoose.connect(url)
+const con = mongoose.connection
+
+con.on('open',()=>
+{
+    console.log('connected...')
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/emp_data',empRouter)
+app.listen(3000,()=>{
+    console.log('Server started')
+})
